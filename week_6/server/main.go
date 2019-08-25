5f35b36be9e3232010b5c58348586267ee6db6be
don tran <trantiendo12@gmail.com>
2019-08-25 17:49:52 +0700

    Add log to echo server

--- week_6/server/main.go
+++ week_6/server/main.go
@@ -8,21 +8,22 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var client = newClient()
-
 func main() {
 
 	// Create our server
 	logger := log.New()
+	client := addDataDogClient()
 	server := Server{
 		logger: logger,
+		client: client,
 	}
-	newClient()
+	server.initLogLevel()
+
 	// Start the server
 	server.ListenAndServe()
 }
 
-func newClient() *statsd.Client {
+func addDataDogClient() *statsd.Client {
 	client, err := statsd.New("127.0.0.1:8125",
 		statsd.WithNamespace("test."),                  // prefix every metric with the app name
 		statsd.WithTags([]string{"region:us-east-1a"}), // send the EC2 availability zone as a tag with every metric
@@ -31,17 +32,23 @@ func newClient() *statsd.Client {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	return client
 }
 
 // Server represents our server.
 type Server struct {
 	logger *log.Logger
+	client *statsd.Client
+}
+
+func (s *Server) initLogLevel() {
+	s.logger.SetLevel(log.InfoLevel)
 }
 
 // ListenAndServe starts the server
 func (s *Server) ListenAndServe() {
-	s.logger.Info("echo server is starting on port 15000...")
+	s.logger.Info("Echo server is starting on port 15000...")
 	http.HandleFunc("/", s.echo)
 	http.ListenAndServe(":15000", nil)
 }
@@ -53,12 +60,14 @@ func (s *Server) echo(writer http.ResponseWriter, request *http.Request) {
 
 	// 30% chance of failure
 	if rand.Intn(100) < 30 {
-		client.Count("fail", 1, []string{"localhost"}, 1)
+		s.logger.Error("Echo server was broken")
+		s.client.Count("fail", 1, []string{"localhost"}, 1)
 		writer.WriteHeader(500)
 		writer.Write([]byte("a chaos monkey broke your server"))
 		return
 	}
-	client.Count("success", 1, []string{"localhost"}, 1)
+	s.logger.Info("Echo server response with success status")
+	s.client.Count("success", 1, []string{"localhost"}, 1)
 	// Happy path
 	writer.WriteHeader(200)
 	request.Write(writer)
