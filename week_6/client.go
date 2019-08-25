cb3438d02a1171d933250bde3bfae157215b7171
don tran <trantiendo12@gmail.com>
2019-08-25 22:02:47 +0700

    Add circuit breaker in retry way

--- week_6/client.go
+++ week_6/client.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/avast/retry-go"
 	hystrix "github.com/myteksi/hystrix-go/hystrix"
@@ -28,11 +32,58 @@ func main() {
 	}
 	client.initLogLevel()
 	client.configureCircuitBreaker()
-	client.beginRequest()
-
+	fmt.Println("Choose 1 if you want to apply retry in circuit breaker - 2 if you want to apply circuit breaker in retry: ")
+	fmt.Print("> ")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		choice, err := strconv.ParseInt(input, 10, 64)
+		if err == nil {
+			if choice == 1 {
+				client.beginRequestWithRetryInCircuitBreaker()
+			} else if choice == 2 {
+				client.beginRequestWithCircuitBreakerInRetry()
+			}
+		}
+	}
 }
 
-func (client *Client) beginRequest() {
+func (client *Client) beginRequestWithCircuitBreakerInRetry() {
+	for i := 1; i <= numberOfSampleRequest; i++ {
+		j := i
+		retry.Do(
+			func() error {
+				client.logger.Info("Request ", j, " begin")
+				err := client.callWithCircuitBreaker(j)
+				return err
+			},
+			retry.Attempts(maxAttempts),
+		)
+	}
+}
+
+func (client *Client) callWithCircuitBreaker(j int) error {
+	err := hystrix.Do(circuitBreakerName, func() error {
+		resp, err := http.Get(url)
+		if resp.StatusCode >= 500 {
+			client.logger.Error("Server error")
+			return errors.New("Server error")
+		}
+		defer resp.Body.Close()
+		_, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			client.logger.Error("Cannot parse response body")
+			return errors.New("Cannot parse response body")
+		}
+		return nil
+	}, func(err error) error {
+		client.logger.Error("request ", j, ": fail, service is not available\n")
+		return errors.New("Fall back")
+	})
+	return err
+}
+
+func (client *Client) beginRequestWithRetryInCircuitBreaker() {
 	for i := 1; i <= numberOfSampleRequest; i++ {
 		j := i
 		hystrix.Do(circuitBreakerName, func() error {
